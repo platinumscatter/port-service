84780ee89371db1ee3be7305c98943cf3f932b1f
platinumscatter <platinumscatter@gmail.com>
2024-12-14 10:09:28 +0300

    port service - take 2

--- cmd/port-service/main.go
+++ cmd/port-service/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/platinumscatter/port-service/internal/config"
 )
 
@@ -15,6 +22,42 @@ func main() {
 }
 
 func run() error {
-	_ = config.Read()
+	cfg := config.Read()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(w).Encode(map[string]bool{
+			"pong": true,
+		})
+	}).Methods("GET")
+
+	srv := &http.Server{
+		Addr:    cfg.HTTPAddr,
+		Handler: router,
+	}
+
+	stopped := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+		<-sigint
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("HTTP Server Shutdown Error: %v", err)
+		}
+		close(stopped)
+	}()
+
+	log.Printf("Starting server HTTP on %s ", cfg.HTTPAddr)
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("HTTP Server ListenAndServe Error: %v", err)
+	}
+	<-stopped
+	log.Printf("Have a nice day!")
+
 	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/platinumscatter/port-service
 
 go 1.23.4
+
+require github.com/gorilla/mux v1.8.1
